9ec7e0d843b25fda2b0cceead35d8665a5d41e6c
Artur Assis Alves <60040236+ArturAssisComp@users.noreply.github.com>
2023-03-29 16:10:14 -0300

    Add phone number, email, and format the interface

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,15 @@ class MiCardFlutter extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
+              SizedBox(
+                width: double.infinity,
+                height: 20.0,
+              ),
               CircleAvatar(
                 foregroundImage: AssetImage('images/profile_artur1.jpg'),
                 backgroundColor: Colors.blue[100],
@@ -25,6 +30,75 @@ class MiCardFlutter extends StatelessWidget {
                   fontSize: 35,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                padding: EdgeInsets.all(
+                  10,
+                ),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+55 61 98301-3819',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 18,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                padding: EdgeInsets.all(
+                  10,
+                ),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email_outlined,
+                      color: Colors.teal,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'arturassiscomp@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 18,
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
